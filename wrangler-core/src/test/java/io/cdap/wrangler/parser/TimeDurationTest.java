703898da4bd4fe4bffec8e3e9e7cb842444ee430
RudreeshJoshi <168192024+RudreeshJoshi@users.noreply.github.com>
2025-05-10 18:21:03 +0530

    Update TimeDurationTest.java
    Time Duration Test Java

--- wrangler-core/src/test/java/io/cdap/wrangler/parser/TimeDurationTest.java
+++ wrangler-core/src/test/java/io/cdap/wrangler/parser/TimeDurationTest.java
@@ -1,9 +1,15 @@
+package io.cdap.wrangler.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TimeDurationTest {
     @Test
     public void testParse() {
-        assertEquals(1000000L, new TimeDuration("1ms").getNanos());
-        assertEquals(1000000000L, new TimeDuration("1s").getNanos());
-        // Add more test cases for different units
+        assertEquals(1_000_000L, new TimeDuration("1ms").getNanos());
+        assertEquals(1_000_000_000L, new TimeDuration("1s").getNanos());
+        assertEquals(60_000_000_000L, new TimeDuration("1min").getNanos());
+        assertEquals(3_600_000_000_000L, new TimeDuration("1hr").getNanos());
+        // Add more test cases as needed
     }
-
 }
